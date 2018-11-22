66783d16584a98d5c31e9de87df1f3ea8eb5f27f
AnghelAndrei28 <45268599+AnghelAndrei28@users.noreply.github.com>
2018-11-22 20:15:46 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Numar_Prim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
